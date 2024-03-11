a9659ad28099b21d9e4d3447564f1427dfd36592
calumtreloar <calumtreloar@gmail.com>
2024-03-11 21:17:52 +1030

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import SlickCarousel from "./assets/components/SlickCarousel";
+// import Carousel from "./assets/components/Carousel";
+
+const App = () => {
+  return (
+    <main>
+      <SlickCarousel />
+    </main>
+  );
+};
+export default App;
